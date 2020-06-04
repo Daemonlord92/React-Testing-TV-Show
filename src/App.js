82713ed6722b9f4e3b78e3c0b2e08444be037c6d
Matthew Martin <zhintateo19924559@gmail.com>
2020-06-03 19:36:13 -0500

    Got tests for the App.js Written

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default function App() {
 
         fetchShows()
             .then(res => {
+                console.log(res)
                 setShow(res.data)
                 setSeasons(formatSeasons(res.data._embedded.episodes))
             });
